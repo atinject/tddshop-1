99471363457c64529a0b49363a4091af933ee3ba
atinjector <kun.cai.rbc@gmail.com>
2016-05-31 13:29:31 +0100

    STEP 1 - added step 1 code and test cases

--- src/main/scala/kuncai/CartService.scala
+++ src/main/scala/kuncai/CartService.scala
@@ -1,21 +1,46 @@
 package kuncai
 
+import scala.annotation.tailrec
+
 /**
   * Created by kcai on 31/05/2016.
   */
-class CartService(items: List[String]) {
+class CartService(items: List[String]) extends ItemRepository {
 
-  def basket: List[Item] = {
-    items.map(n => Item(n, 0.00))
+  def basket = items.foldLeft(List.empty[Item])((savedItems: List[Item], x: String) =>
+    getItem(x) match {
+      case Some(e) => e :: savedItems
+      case None => savedItems
+    }
+  )
+
+  def checkOut = {
+    @tailrec
+    def sum(savedItems: List[Item], total: BigDecimal): BigDecimal = {
+      savedItems match {
+        case Nil => total
+        case x :: tail => sum(tail, total + x.price)
+      }
+    }
+    sum(basket, 0.00)
   }
-
-  def checkout = {
-
-  }
-
 }
-
 
 case class Item(name: String, price: BigDecimal) {
   override val toString = name
+}
+
+object ItemRepository {
+  val items = Map(
+    "APPLE" -> Item("APPLE", 0.60),
+    "ORANGE" -> Item("ORANGE", 0.25)
+  )
+}
+
+sealed trait ItemRepository {
+  import ItemRepository._
+
+  def getItem(name: String) = {
+    items.get(name)
+  }
 }
--- src/test/scala/kuncai/CartTest.scala
+++ src/test/scala/kuncai/CartTest.scala
@@ -23,5 +23,40 @@ class CartTest extends FlatSpec with Matchers {
     cartService.basket.size should be(3)
   }
 
+  "STEP 1 - A checkout system" should "have Apple priced at 0.60" in {
+    val cartService = new CartService(List("APPLE"))
+    cartService.basket.find(_.name == "APPLE") match {
+      case Some(x) => x.price should be(0.60)
+      case None => fail("No correct item found")
+    }
+  }
+
+  "STEP 1 - A checkout system" should "have Orange priced at 0.25" in {
+    val cartService = new CartService(List("ORANGE"))
+    cartService.basket.find(_.name == "ORANGE") match {
+      case Some(x) => x.price should be(0.25)
+      case None => fail("No correct item found")
+    }
+  }
+
+  "A checkout system" should "calculate the basket total" in {
+    val cartService = new CartService(List("APPLE", "APPLE", "APPLE","ORANGE"))
+    cartService.checkOut should be(2.05)
+  }
+
+  "A checkout system" must "calculate the total price of saved items in basket Apple only" in {
+    val cartService = new CartService(List("APPLE"))
+    cartService.checkOut should be(0.60)
+  }
+
+  "A checkout system" must "calculate the total price of saved items in basket Orange only" in {
+    val cartService = new CartService(List("ORANGE"))
+    cartService.checkOut should be(0.25)
+  }
+
+  "A checkout system" must "calculate the total price of saved items in basket with mixed selections" in {
+    val cartService = new CartService(List("ORANGE", "APPLE", "ORANGE"))
+    cartService.checkOut should be(1.10)
+  }
 
 }
